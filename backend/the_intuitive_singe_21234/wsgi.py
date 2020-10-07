c4d275ed1405165d7da70d074c8a80c2463c28f1
Crowdbotics <team@crowdbotics.com>
2020-10-07 21:14:59 +0000

    Update models

--- backend/the_intuitive_singe_21234/wsgi.py
+++ backend/the_intuitive_singe_21234/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_intuitive_singe_21234.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_intuitive_singe_21234.settings")
 
 application = get_wsgi_application()
